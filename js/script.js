91f61a13e472d1cb8de75c7c6e50cf0e4a441903
hostech83 <166859198+hostech83@users.noreply.github.com>
2024-05-02 11:29:35 -0400

    Update script.js
    Delete the content of script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-alert('Hello world');
-
-// Define the variable favoriteFood and assign it a value
-var favoriteFood = "Pizza";
-
-
-// Define the variable favoriteFood and assign it a value
-var favoriteFood = "Pizza";
-
-// Display the value of favoriteFood on the index page
-document.write("My favorite food is: " + favoriteFood);
-// Display the value of favoriteFood on the index page
-document.write("My favorite food is: " + favoriteFood);
-
 
