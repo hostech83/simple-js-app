8bf49363d0242d5caa90d8249fa85f018a85251c
hostech83 <166859198+hostech83@users.noreply.github.com>
2024-05-09 03:50:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let pokemonRepository = (function () {
     let pokemonList = [
-<<<<<<< HEAD
         //Pokemon data here
         {
             name: "Bulbasaur",
@@ -18,24 +17,6 @@ let pokemonRepository = (function () {
             types: ['Water']
         }
     ];
-=======
-    {
-        name: "Bulbasaur",
-        height: 7,
-        types: ['Grass', 'Poison']
-    },
-    {
-        name: "Charizard",
-        height: 6,
-        types: ['Fire']
-    },
-    {
-        name: "Squirtle",
-        height: 5,
-        types: ['Water']
-    }
-];
->>>>>>> 092bc13f4ce415899a308a54c074a1e900f8b23a
     return {
         getAll: function () {
             return pokemonList;
@@ -44,7 +25,6 @@ let pokemonRepository = (function () {
         add: function (pokemon) {
             pokemonList.push(pokemon);
         }
-<<<<<<< HEAD
 
     };
 })(); // <-- This line invokes the IIFE
@@ -52,13 +32,3 @@ let pokemonRepository = (function () {
 pokemonRepository.getAll().forEach(function (pokemon) {
     document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
 });
-=======
-        
-    },
-})();
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + " (height: " + pokemon.height +")<br>");
-})
-
->>>>>>> 092bc13f4ce415899a308a54c074a1e900f8b23a
